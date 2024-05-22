b608630cf62e0781044f19e4529b31ddab1fdc8c
deividasmalcevas <deividasmalcevas@gmail.com>
2024-05-22 12:12:23 +0300

    admin login create movie

--- global_functions.js
+++ global_functions.js
@@ -32,3 +32,74 @@ export function user_login(user){
 export function logged_in(){
     return get_local_storage(`logged`)
 }
+export function check(title, img, seats) {
+    const fields = [title, img, seats];
+    fields.forEach(field => change_input_bg(field, 'white'));
+
+    if (!fields.every(field => field.value)) {
+        fields.filter(field => !field.value).forEach(field => change_input_bg(field, 'red'));
+        alert('Missing fields');
+        return false;
+    }
+    if (title.value.length > 300) {
+        change_input_bg(title, 'red');
+        alert('Title too long');
+        return false;
+    }
+    if (!valid_img(img.value)) {
+        change_input_bg(img, 'red');
+        alert('Please enter a valid image URL.');
+        return false;
+    }
+    const seatsValue = parseInt(seats.value, 10);
+    if (seatsValue < 1 || seatsValue > 100) {
+        change_input_bg(seats, 'red');
+        alert('There can only be 1-100 seats.');
+        return false;
+    }
+    return true;
+}
+function change_input_bg(input, color){
+   input.style.backgroundColor =  color
+}
+function valid_img(url) {
+    const imageExtensions = /\.(jpeg|jpg|gif|png|svg|webp|bmp|tiff?)$/i;
+    return imageExtensions.test(url);
+}
+export function create_movie(title, img, seats) {
+    let movies = get_local_storage('movies') || [];
+
+    movies.push({ title: title, img: img, seats: seats });
+
+    create_local_storage('movies', movies);
+
+    window.location.href = 'movie_list.html';
+}
+export function display_movies(div){
+    let movies = get_local_storage('movies') || [];
+    movies.forEach((mov) =>{
+        const movie = document.createElement('div');
+        movie.classList.add('movie');
+
+        const movie_img = document.createElement('div');
+        movie_img.classList.add('movie_img');
+        movie_img.style.backgroundImage = `url("${mov.img}")`
+        const movie_info = document.createElement('div');
+        movie_info.innerHTML = `<h5>${mov.title}</h5><h6>Available seats: ${mov.seats}</h6>`
+        movie.onclick = () => {
+
+        }
+        movie.appendChild(movie_img)
+        movie.appendChild(movie_info)
+        if(logged_in() === `admin`){
+            const btn_del = document.createElement('button');
+            btn_del.classList.add('btn', `btn-danger`);
+            btn_del.textContent = `remove movie`
+            btn_del.onclick = () => {
+
+            }
+            movie.appendChild(btn_del)
+        }
+        div.appendChild(movie)
+    });
+}
--- main.js
+++ main.js
@@ -1,26 +1,59 @@
 //imports
-import { user_login, logged_in } from './global_functions.js';
+import { user_login, logged_in, check, create_movie, display_movies } from './global_functions.js';
 
 //elements from HTML
 const logged = document.getElementById(`logged_in`);
+const movie_list = document.getElementById(`movie_list`);
+//inputs
+const movie_title = document.getElementById(`movieTitle`);
+const movie_image = document.getElementById(`movieImage`);
+const seats = document.getElementById(`seatsTotal`);
 //btns
 const btn_login_user = document.getElementById(`btn_login_user`);
 const btn_login_admin = document.getElementById(`btn_login_admin`);
+const btn_logout = document.getElementById(`btn_logout`);
+const btn_create_movie = document.getElementById(`btn_create_movie`);
 
+//vars
+let logged_user
 //btns clicks
 if(btn_login_user){
     btn_login_user.onclick = () => {
         user_login(`user`)
-        window.location.href = 'login.html';
+        window.location.href = 'movie_list.html';
     }
 }
 if(btn_login_admin){
     btn_login_admin.onclick = () => {
         user_login(`admin`)
-        window.location.href = 'login.html';
+        window.location.href = 'movie_list.html';
+    }
+}
+if(btn_logout){
+    btn_logout.onclick = () => {
+        window.location.href = 'index.html';
+    }
+}
+if(btn_create_movie){
+    btn_create_movie.onclick = () => {
+        if(check(movie_title,movie_image, seats)) {
+            create_movie(movie_title.value,movie_image.value, seats.value)
+        }
     }
 }
 if(logged){
-    logged.textContent = logged_in()
+    logged_user = logged_in()
+    logged.textContent = logged_user
+    display_movies(movie_list)
+    if(logged_user === `admin`){
+        const movie = document.createElement('div');
+        movie.classList.add('movie');
+        movie.style.textAlign = `center`
+        movie.textContent = `ADD new movie`;
+        movie.onclick = () => {
+            window.location.href = 'add_movie.html';
+        }
+        movie_list.appendChild(movie)
+    }
 }
 
